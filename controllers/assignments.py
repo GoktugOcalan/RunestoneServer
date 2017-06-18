0c6a1de10f2025d57ae6a9fc2ce29063dea83cc7
GoktugOcalan <ocalang@itu.edu.tr>
2017-06-18 15:04:50 +0300

    Added shop function
    Added the new function shop. This function is called when the newly added "Shop Page" is being loaded. Function handles authentication. Then it gets the information for the current student and returns it in the returning dict. It also returns students current points in the variable points. It later returns the unlockable feature list for the student in the unlockables variable. In the "Shop Page" points value is displayed and a list of features are shown. Features are grayed out if they are not affordable or if they are already unlocked.

--- controllers/assignments.py
+++ controllers/assignments.py
@@ -93,6 +93,36 @@ def index():
         achievements = achievements,
         )
 
+def shop():
+    if not auth.user:
+        session.flash = "Please Login"
+        return redirect(URL('default','index'))
+    if 'sid' not in request.vars and verifyInstructorStatus(auth.user.course_name, auth.user):
+        return redirect(URL('assignments','admin'))
+    if 'sid' not in request.vars:
+        return redirect(URL('assignments','shop') + '?sid=%d' % (auth.user.id))
+    if str(auth.user.id) != request.vars.sid and not verifyInstructorStatus(auth.user.course_name, auth.user):
+        return redirect(URL('assignments','shop'))
+    student = db(db.auth_user.id == request.vars.sid).select(
+        db.auth_user.id,
+        db.auth_user.username,
+        db.auth_user.first_name,
+        db.auth_user.last_name,
+        db.auth_user.email,
+        ).first()
+    if not student:
+        return redirect(URL('assignments','shop'))
+    
+    points = db(db.user_points.user_id == auth.user.id).select(db.user_points.points).first().points
+    
+    unlockables = db(db.unlockables.user_id == auth.user.id).select().first()
+    
+    return dict(
+        student = student,
+        points = points,
+        unlockables = unlockables,
+        )
+
 @auth.requires(lambda: verifyInstructorStatus(auth.user.course_name, auth.user), requires_login=True)
 def admin():
     course = db(db.courses.id == auth.user.course_id).select().first()
